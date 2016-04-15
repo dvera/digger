1bec39331346897f4bf8ed66d07e6588b99ea337
Daniel Vera <dlv04c@spear-50.local>
2016-04-15 10:46:46 -0400

    limma_xys

--- R/dge.heatmap.R
+++ R/dge.heatmap.R
@@ -1,4 +1,4 @@
-dge.heatmap <- function ( counts , genelists , savetables=FALSE , listnames=NULL, samples=NULL, color=colorRampPalette(c("black","blue","yellow","red"))  ){
+dge.heatmap <- function ( counts , genelists , suffix="" , savetables=FALSE , sortby=1, hcluster=FALSE , PDF=TRUE, listnames=NULL, samples=NULL, color=colorRampPalette(c("black","blue","yellow","red"))  ){
 
   library(edgeR)
 
@@ -22,15 +22,28 @@ dge.heatmap <- function ( counts , genelists , savetables=FALSE , listnames=NULL
   numlists <- length(genelists)
   lists <- lapply(lapply( genelists, read_tsv, col_names=F ),unlist)
   genes <- lapply(1:numlists,function(x){
-    unique(toupper(lists[[x]]))
+    gene <- toupper(lists[[x]])
+    gene <- gsub("Α","A",gene)
+    gene <- gsub(" ","",gene)
+    gene <- gsub("-","",gene)
+    gene <- unique(gene)
+    gene
   })
+
+
   generows <- lapply(1:numlists, function(x){
-    which(toupper(row.names(cnts)) %in% toupper(gsub(" ","",gsub("Α","A",genes[[x]]))))
+    which(row.names(cnts) %in% genes[[x]])
+  })
+  badgenes <- lapply(1:numlists, function(x){
+    genes[[x]][which(genes[[x]] %ni% row.names(cnts) ) ]
+
   })
 
 
 
-  pdf(file=paste0(basename(removeext(counts)),"_log1pRPKM_heatmaps.pdf"),width=12,height=12)
+  if(PDF){
+    pdf(file=paste0(basename(removeext(counts)),"_log1pRPKM_heatmaps",suffix,".pdf"),width=12,height=12)
+  }
 
   for(i in seq_len(numlists)){
 
@@ -39,12 +52,23 @@ dge.heatmap <- function ( counts , genelists , savetables=FALSE , listnames=NULL
     subrpkms <- rpkms[generows[[i]],]
     if(savetables){ write.tsv(subrpkms, file=paste0(basename(removeext(counts)),"_",listnames[i],".tsv") ,colnames=T, rownames=T)}
 
-
-    heatmap.2(sublog1rpkms,trace="none",Colv=F,main=paste(listnames[i]), col=color)
+    sublog1rpkms <- sublog1rpkms[order(sublog1rpkms[,sortby],decreasing=F),]
+    #sublog1rpkms <- sublog1rpkms[order(round(sublog1rpkms[,1]),round(sublog1rpkms[,2]),decreasing=F),]
+    if(!PDF){
+      tiff(file=paste0(basename(removeext(counts)),"_log1pRPKM_heatmaps",suffix,"_",listnames[i],".tiff"),width=400,height=1000)
+    }
+    heatmap.2(sublog1rpkms,trace="none",Colv=F,Rowv=if(hcluster){TRUE} else{FALSE},main=paste(listnames[i],"(",nrow(sublog1rpkms),")"), col=color)
+    if(!PDF){
+      dev.off()
+    }
+    xx=as.data.frame(sublog1rpkms)
+    print(xx)
+    write.tsv(xx,file=paste0(basename(removeext(counts)),"_log1pRPKM",suffix,"_",listnames[i],".tsv"),rownames=T,colnames=T)
     #heatmap.2(log1rpkms[generows[[i]],],trace="none",Rowv=F,Colv=F,main=paste(listnames[i],"log( 1 + CPM )"))
   }
 
   dev.off()
 
+  print(badgenes)
 
 }
---
+++ R/limma_xys.R
@@ -0,0 +1,98 @@
+limma_xys <- function( xysFiles , grouping=NULL , annos=NULL , pval=0.05 ){
+
+  if(is.data.frame(xysFiles)){
+    grp=xysFiles[,2]
+    xys=xysFiles[,1]
+  } else{
+    grp=grouping
+  }
+
+  numfiles = length(xys)
+
+  ug <- unique(grp)
+
+  design <- matrix(rep(0,length(grp)*2),ncol=2)
+  design[which(grp==ug[1]),1]<-1
+  design[which(grp==ug[2]),2]<-1
+  colnames(design)<-ug
+
+  maqc=read.xysfiles(xys)
+  eset=rma(maqc)
+  fit <- lmFit(eset, design)
+  cont.matrix <- makeContrasts(test=paste0(ug[2],"-",ug[1]), levels=design)
+  fit2 <- contrasts.fit(fit, cont.matrix)
+  eb <- eBayes(fit2)
+  tt <- topTable(eb,n=5000000 )
+  e <- exprs(eset)
+
+  tt <- tt[order(rownames(tt)),]
+  e <- e[order(rownames(e)),]
+
+
+
+  newt=cbind(probe=rownames(e),e,tt)
+  if(!identical(colnames(newt)[2:(numfiles+1)],basename(xys))){stop("somethings wrong")}
+
+  colnames(newt)[2:(numfiles+1)] <- paste0(grp,"__",basename(xys))
+
+
+  if(!is.null(annos)){
+
+
+    anno=tsvRead(annos)
+
+    if(is.data.frame(anno)){
+      anno <- list(anno)
+    }
+
+    for(i in 1:length(annos)){
+      annos2=anno[[i]][,c(4,7)]
+      annos3=unique(annos2)
+      annos4=aggregate(annos3[,2],by=list(annos3[,1]),FUN=paste0)
+      m=match(newt$probe,annos4[,1])
+      newt[[basename(removeext(annos[i]))]] <- annos4[m,2]
+      #newt2=merge(newt,annos4,by.x="probe",by.y="Group.1",all.x=T,all.y=F)
+      newt[[basename(removeext(annos[i]))]] <- unlist(lapply(newt[[basename(removeext(annos[i]))]],paste0,collapse=","))
+      #colnames(newt)[which(colnames(newt)=="x")] <- "xenoRef"
+  }
+
+  pos24=newt[which(newt$P.Value<=pval & newt$logFC >=2 & newt$logFC < 4 ),]
+  neg24=newt[which(newt$P.Value<=pval & newt$logFC <=(-2) & newt$logFC > -4 ),]
+  neg4=newt[which(newt$P.Value<=pval & newt$logFC < -4 ),]
+  pos4=newt[which(newt$P.Value<=pval & newt$logFC > 4 ),]
+
+  outname=paste0(ug[1],"_vs_",ug[2],".tsv")
+  pos24name=paste0(ug[1],"_vs_",ug[2],"_upreg_logFc2to4.tsv")
+  neg24name=paste0(ug[1],"_vs_",ug[2],"_downreg_logFc2to4.tsv")
+  pos4name=paste0(ug[1],"_vs_",ug[2],"_upreg_logFcGreaterThan4.tsv")
+  neg4name=paste0(ug[1],"_vs_",ug[2],"_downreg_logFcGreaterThan4.tsv")
+
+  tsvWrite(newt,file=outname,col_names=T)
+  if(nrow(pos24)>0){ tsvWrite(pos24,file=pos24name,col_names=T) }
+  if(nrow(neg24)>0){ tsvWrite(neg24,file=neg24name,col_names=T) }
+  if(nrow(pos4)>0){ tsvWrite(pos4,file=pos4name,col_names=T) }
+  if(nrow(neg4)>0){ tsvWrite(neg4,file=neg4name,col_names=T) }
+
+
+}
+
+
+
+
+  # par(mfrow=c(2,2))
+  #
+  # x1= rowMeans(e[,which(design[,1]==1)])
+  # x2= rowMeans(e[,which(design[,2]==1)])
+  #
+  # plot(x1,x2,col=tt$color,pch=20,xlab=ug[1],ylab=ug[2])
+  # plot(tt$AveExpr,tt$logFC,col=tt$color,xlab="mean expression",ylab="log2 ratio")
+  # plot(density(as.vector(e)))
+  # plot(density(as.vector(e)))
+
+
+
+  # tt$color="black"
+  # tt$color[which(tt$logFC<0 & tt$P.Value <=0.05)]<-"darkred"
+  # tt$color[which(tt$logFC<0 & tt$adj.P.Val <=0.05)]<-"darkred"
+  # tt$color[which(tt$logFC>0 & tt$P.Value <=0.05)]<-"darkgreen"
+  # tt$color[which(tt$logFC>0 & tt$adj.P.Val <=0.05)]<-"green"

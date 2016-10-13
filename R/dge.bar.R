43f9267b3a3087729240bb4699f6c879f67b376c
Kathleen Kyle <kkyle@pauper.bio.fsu.edu>
2016-10-13 16:25:19 -0400

    various

--- R/dge.bar.R
+++ R/dge.bar.R
@@ -1,4 +1,9 @@
-dge.log2 <- function( dges , genelists=NULL , listnames=NULL , sortByChange=TRUE , debug=getOption("verbose"), cuffdiff=FALSE, significantOnly=FALSE, savetables=FALSE ){
+# significantOnly option
+### 0 = false
+### 1 = dark and light color bars (Qval <= 0.1)
+### 2 = only light color bars (most significant, Qval <= 0.05)
+
+dge.bar <- function( dges , genelists=NULL , listnames=NULL , sortByChange=TRUE , debug=getOption("verbose"), cuffdiff=FALSE, significantOnly=0, savetables=FALSE, logTwo=TRUE ){
 
   #library(edgeR)
   #library(DESeq2)
@@ -13,6 +18,14 @@ dge.log2 <- function( dges , genelists=NULL , listnames=NULL , sortByChange=TRUE
   # data(go.sets.hs)
   # data(go.subs.hs)
   # data(kegg.gs)
+  if(significantOnly < 0) {
+    cat(">> Invalid significantOnly Value! Defaulting to 0\n")
+    significantOnly = 0
+  } else if(significantOnly > 2) {
+    cat(">> Invalid significantOnly Value! Defaulting to 2\n")
+    significantOnly = 2
+  }
+
   numdes <- length(dges)
 
   if(is.null(listnames)){
@@ -21,7 +34,12 @@ dge.log2 <- function( dges , genelists=NULL , listnames=NULL , sortByChange=TRUE
 
 
   for(d in seq_len(numdes)){
-    pdf(file=paste0(basename(removeext(dges[d])),"_log2bar.pdf"))
+    if(logTwo) {
+      ext="_log2bar.pdf"
+    } else {
+      ext="_bar.pdf"
+    }
+    pdf(file=paste0(basename(removeext(dges[d])),"_sig",significantOnly,ext))
     de <- read.tsv(dges[d],header=T)
 
     if(cuffdiff){
@@ -83,26 +101,55 @@ dge.log2 <- function( dges , genelists=NULL , listnames=NULL , sortByChange=TRUE
 
 
       if(sortByChange){subde<-subde[order(subde$logFC, decreasing=T),]}
-      if(significantOnly){subde<-subde[-which(subde$color=="black"),]}
+      if(significantOnly > 0) {
+        w=which(subde$color=="black")
+        if(length(w) != 0)
+          subde<-subde[-w,]
+        if(significantOnly == 2) {
+          w=which(subde$color=="darkgreen")
+          if(length(w) != 0)
+            subde<-subde[-w,]
+          w=which(subde$color=="darkred")
+          if(length(w) != 0)
+            subde<-subde[-w,]
+        }
+      }
 
-      if(savetables){ write.tsv(subde, file=paste0(basename(removeext(dges[d])),"_",listnames[i],".edger"), colnames=T )}
+      if(savetables){ write.tsv(subde, file=paste0(basename(removeext(dges[d])),"_",removeext(listnames[i]),".edger"), colnames=T )}   # eh....
 
 
       if(nrow(subde)>0){
-        barplot(
-          subde$logFC,
-          #log1cnts[,which(colnames(cnts)==eg[g,2])],
-          main=listnames[i],
-          col=subde$color,
-          names=subde$gene,
-          las=3,
-          #pch=20,
-          #xlab="log2(mean of counts)",
-          ylab=paste0("log2(",colnames(de)[3],"/",colnames(de)[2],")")
-        )
-        abline(h=0,v=(1:nrow(subde)*1.2) , col=rgb(0,0,0,20,maxColorValue=255) )
-        #par(mar=c(2,2,2,1), xpd=FALSE)
-
+        if(logTwo) {
+          barplot(
+            subde$logFC,
+            #log1cnts[,which(colnames(cnts)==eg[g,2])],
+            main=removeext(listnames[i]),
+            col=subde$color,
+            names=subde$gene,
+            las=3,
+            #pch=20,
+            #xlab="log2(mean of counts)",
+            #ylab=paste0("log2(",colnames(de)[3],"/",colnames(de)[2],")")
+            ylab="log2(Fold Change)"
+          )
+          abline(h=0,v=(1:nrow(subde)*1.2) , col=rgb(0,0,0,20,maxColorValue=255) )
+          #par(mar=c(2,2,2,1), xpd=FALSE)
+        } else {
+          barplot(
+            2^subde$logFC,
+            #log1cnts[,which(colnames(cnts)==eg[g,2])],
+            main=removeext(listnames[i]),
+            col=subde$color,
+            names=subde$gene,
+            las=3,
+            #pch=20,
+            #xlab="log2(mean of counts)",
+            #ylab=paste0(colnames(de)[3],"/",colnames(de)[2],")")
+            ylab="Fold Change"
+          )
+          abline(h=0,v=(1:nrow(subde)*1.2) , col=rgb(0,0,0,20,maxColorValue=255) )
+          #par(mar=c(2,2,2,1), xpd=FALSE)
+        }
         barplot(
           log10(subde$QValue),
           #log1cnts[,which(colnames(cnts)==eg[g,2])],
@@ -113,7 +160,7 @@ dge.log2 <- function( dges , genelists=NULL , listnames=NULL , sortByChange=TRUE
           las=3,
           #pch=20,
           #xlab="log2(mean of counts)",
-          ylab="log10( p-value )"
+          ylab="log10( q-value )"
         )
 
         abline(h=c(-1,-1.3), v=(1:nrow(subde)*1.2) , col=rgb(0,0,0,20,maxColorValue=255) )
